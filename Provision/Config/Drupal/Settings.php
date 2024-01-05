41d9adbaf307c23a2a1e4567f9db100d4cb5ce2c
Mathieu Lu <mathieu@symbiotic.coop>
2024-01-05 21:37:01 +0000

    Provision/Config/Drupal/Settings.php: D10 fix for version detection and DRUSH_DRUPAL_ROOT
    Not sure about DRUSH_DRUPAL_ROOT, but "works for me"(tm).
    For the version detection, it's a global issue.

--- Provision/Config/Drupal/Settings.php
+++ Provision/Config/Drupal/Settings.php
@@ -15,12 +15,13 @@ class Provision_Config_Drupal_Settings extends Provision_Config {
   }
 
   function process() {
-    if (drush_drupal_major_version() == 10) {
+    $drupal_core_version = provision_get_drupal_core_version();
+    if ($drupal_core_version && version_compare($drupal_core_version, 10, '>=')) {
       $this->template = 'provision_drupal_settings_10.tpl.php';
       $this->data['db_type'] = ($this->data['db_type'] == 'mysqli') ? 'mysql' : $this->data['db_type'];
       $this->data['utf8mb4_is_configurable'] = TRUE;
       $this->data['utf8mb4_is_supported'] = $this->db_server->utf8mb4_is_supported;
-      $drupal_root = drush_get_context('DRUSH_DRUPAL_ROOT');
+      $drupal_root = d()->root;
       require_once $drupal_root . '/core/lib/Drupal/Component/Utility/Crypt.php';
       $this->data['drupal_hash_salt_var'] = Drupal\Component\Utility\Crypt::randomBytesBase64(55);
       $this->data['maintenance_var_new'] = TRUE;
