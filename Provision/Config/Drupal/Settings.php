55379d3f6d0fb83f953fb29f8246df348ad7fc82
Mathieu Lu <mathieu@symbiotic.coop>
2024-03-30 16:23:36 +0000

    Provision/Config/Drupal/Settings.php: workaround a mysterious loss of db credentials

--- Provision/Config/Drupal/Settings.php
+++ Provision/Config/Drupal/Settings.php
@@ -15,6 +15,12 @@ class Provision_Config_Drupal_Settings extends Provision_Config {
   }
 
   function process() {
+    // Protect against mysterious loss of db credentials
+    if (empty($this->data['db_name'])) {
+      $data = d()->service('db')->fetch_site_credentials();
+      $this->data = array_merge($this->data, $data);
+    }
+
     $drupal_core_version = provision_get_drupal_core_version();
     if ($drupal_core_version && version_compare($drupal_core_version, 10, '>=')) {
       $this->template = 'provision_drupal_settings_10.tpl.php';
