40487d3e85149501764610e9bbce9da57b0fb00d
Mathieu Lu <mathieu@symbiotic.coop>
2024-05-10 15:41:49 +0000

    Remove buggy GRANT RELOAD, which if I understand correctly, was finally fixed in MySQL8

--- db/Provision/Service/db/mysql.php
+++ db/Provision/Service/db/mysql.php
@@ -90,12 +90,7 @@ class Provision_Service_db_mysql extends Provision_Service_db_pdo {
       drush_log(dt("Failed to alter db_user @name", array('@name' => $username)), 'error');
       return $user_altered;
     }
-
     $statement = "GRANT ALL PRIVILEGES ON `%s`.* TO `%s`@`%s`";
-
-    // @todo [ML] SYMBIOTIC see https://git.drupalcode.org/project/provision/-/commit/f0e7cda9382471bbc47e34547985b09aa2a35075
-    // MySQL did this to us. https://github.com/drush-ops/drush/issues/5368#issuecomment-1405209770
-    $statement .= "; GRANT RELOAD ON `%s`.* TO `%s`@`%s`";
     return $this->query($statement, $name, $username, $host);
   }
 
