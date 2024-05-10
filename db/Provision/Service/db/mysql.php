bfb74799dad7ff8ecf8b021d9c45f3734cedb870
Mathieu Lu <mathieu@symbiotic.coop>
2024-05-10 15:41:14 +0000

    More ProxySQL cleanup

--- db/Provision/Service/db/mysql.php
+++ db/Provision/Service/db/mysql.php
@@ -146,117 +146,16 @@ class Provision_Service_db_mysql extends Provision_Service_db_pdo {
     return $success;
   }
 
-
   function import_dump($dump_file, $creds) {
     if (empty($creds)) {
       $creds = $this->generate_site_credentials();
     }
     extract($creds);
-    drush_log(dt("DEBUG MyQuick import_dump mysql.php db_name first @var", array('@var' => $db_name)), 'info');
-    $mydumper_path = '/usr/local/bin/mydumper';
-    $myloader_path = '/usr/local/bin/myloader';
-    $script_user = d('@server_master')->script_user;
-    $aegir_root = d('@server_master')->aegir_root;
-    $backup_path = d('@server_master')->backup_path;
-    $oct_db_dirx = $backup_path . '/tmp_expim';
-    $pass_php_inc = $aegir_root . '/.' . $script_user . '.pass.php';
-    drush_log(dt("DEBUG MyQuick import_dump mysql.php pass_php_inc @var", array('@var' => $pass_php_inc)), 'info');
-    $enable_myquick = $aegir_root . '/static/control/MyQuick.info';
-    drush_log(dt("DEBUG MyQuick import_dump mysql.php enable_myquick @var", array('@var' => $enable_myquick)), 'info');
-    if (is_file($enable_myquick) && is_executable($myloader_path)) {
-
-      if (provision_file()->exists($pass_php_inc)->status()) {
-        include_once($pass_php_inc);
-      }
-
-      if ($db_name) {
-        $mycnf = $this->generate_mycnf();
-        $oct_db_user = $db_user;
-        $oct_db_pass = $db_passwd;
-        $oct_db_host = $db_host;
-        $oct_db_port = $db_port;
-
-        if ($this->server->db_port == '6033') {
-          if (is_readable('/opt/tools/drush/proxysql_adm_pwd.inc')) {
-            include('/opt/tools/drush/proxysql_adm_pwd.inc');
-            if ($writer_node_ip) {
-              drush_log('Skip ProxySQL in import_dump', 'notice');
-              $oct_db_host = $writer_node_ip;
-              $oct_db_port = '3306';
-            }
-            else {
-              drush_log('Using ProxySQL in import_dump', 'notice');
-            }
-          }
-        }
-      }
-      else {
-        drush_log(dt("DEBUG MyQuick import_dump mysql.php FAIL no db_name @var", array('@var' => $db_name)), 'info');
-      }
-
-      if (!is_dir($oct_db_dirx)) {
-        drush_log(dt("DEBUG MyQuick import_dump mysql.php fail oct_db_dirx @var", array('@var' => $oct_db_dirx)), 'info');
-        drush_set_error('PROVISION_DB_IMPORT_FAILED', dt('Database import failed (dir: %dir)', array('%dir' => $oct_db_dirx)));
-      }
-
-      $threads = provision_count_cpus();
-      $threads = intval($threads / 4) + 1;
-      drush_log(dt("DEBUG MyQuick import_dump mysql.php db_name second @var", array('@var' => $db_name)), 'info');
-      drush_log(dt("DEBUG MyQuick import_dump mysql.php oct_db_user @var", array('@var' => $oct_db_user)), 'info');
-      drush_log(dt("DEBUG MyQuick import_dump mysql.php oct_db_pass @var", array('@var' => $oct_db_pass)), 'info');
-      drush_log(dt("DEBUG MyQuick import_dump mysql.php oct_db_host @var", array('@var' => $oct_db_host)), 'info');
-      drush_log(dt("DEBUG MyQuick import_dump mysql.php oct_db_port @var", array('@var' => $oct_db_port)), 'info');
-
-      // Create pre-db-import flag file.
-      $pre_import_flag = $backup_path . '/.pre_import_flag.pid';
-      $pre_import_flag_blank = "Starting Import \n";
-      $local_description = 'Adding Pre-DB-Import Flag-File import_dump mysql.php';
-      if (!provision_file()->exists($pre_import_flag)->status()) {
-        provision_file()->file_put_contents($pre_import_flag, $pre_import_flag_blank)
-      	->succeed('Generated blank ' . $local_description)
-      	->fail('Could not generate ' . $local_description);
-      }
-
-      if (is_dir($oct_db_dirx) &&
-        $db_name &&
-        $oct_db_user &&
-        $oct_db_pass &&
-        $oct_db_host &&
-        $oct_db_port) {
-        $command = sprintf($myloader_path . ' --database=' . $db_name . ' --host=' . $oct_db_host . ' --user=' . $oct_db_user . ' --password=' . $oct_db_pass . ' --port=' . $oct_db_port . ' --directory=' . $oct_db_dirx . ' --threads=' . $threads . ' --overwrite-tables --verbose=1');
-        drush_log(dt("DEBUG MyQuick import_dump mysql.php Cmd @var", array('@var' => $command)), 'info');
-        drush_shell_exec($command);
-
-        if (!$command) {
-          drush_set_error('PROVISION_DB_IMPORT_FAILED', dt('Database import failed (%command)', array('%command' => $command)));
-        }
-
-        // Delete pre-db-import flag file.
-        provision_file()->unlink($pre_import_flag)
-          ->succeed('Remove Pre-DB-Import Flag-File')
-          ->fail('Could not remove Pre-DB-Import Flag-File');
-
-		// Create post-db-import flag file.
-		$post_import_flag = $backup_path . '/.post_import_flag.pid';
-		$post_import_flag_blank = "Post-DB-Import \n";
-		$local_description = 'Adding Post-DB-Import Flag-File import_dump mysql.php';
-		if (!provision_file()->exists($post_import_flag)->status()) {
-		  provision_file()->file_put_contents($post_import_flag, $post_import_flag_blank)
-			->succeed('Generated blank ' . $local_description)
-			->fail('Could not generate ' . $local_description);
-		}
-      }
-    }
-    else {
-      $cmd = sprintf("mysql --defaults-file=/dev/fd/3 --force %s", escapeshellcmd($db_name));
-
-      $success = $this->safe_shell_exec($cmd, $db_host, $db_user, $db_passwd, $dump_file);
-
-      drush_log(sprintf("Importing database using command: %s", $cmd));
-
-      if (!$success) {
-        drush_set_error('PROVISION_DB_IMPORT_FAILED', dt("Database import failed: %output", array('%output' => $this->safe_shell_exec_output)));
-      }
+    $cmd = sprintf("mysql --defaults-file=/dev/fd/3 --force %s", escapeshellcmd($db_name));
+    $success = $this->safe_shell_exec($cmd, $db_host, $db_user, $db_passwd, $dump_file);
+    drush_log(sprintf("Importing database using command: %s", $cmd));
+    if (!$success) {
+      drush_set_error('PROVISION_DB_IMPORT_FAILED', dt("Database import failed: %output", array('%output' => $this->safe_shell_exec_output)));
     }
   }
 
